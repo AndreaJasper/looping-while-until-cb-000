e474c80f539e21510c8c9d9265b6fd89e661456d
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-10 03:39:41 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,7 @@
 def using_until
   levitation_force = 6
-
+    counter = 0
+      puts "You need a levigation force of 10 to succeed. You have a levitation force of #{counter}"
+      count += 1
 
 end
