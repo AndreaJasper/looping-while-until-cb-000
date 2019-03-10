dd03ed29010a422972bc67e4c96c0a40256ade99
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-03-10 03:43:18 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 6
     counter = 0
     until counter == 10
-      puts "You need a levigation force of 10 to succeed. You have a levitation force of #{counter}"
+      puts "YWingardium Leviosa"
       counter += 1
     end
 end
